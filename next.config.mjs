899a8644da29e6e21e231f37e92ade366e3137e8
Thalyson <thalison.wilker@gmail.com>
2025-08-24 17:55:11 -0300

    feat: atualizar basePath e assetPrefix para o domínio correto

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: process.env.NODE_ENV === 'production' ? '/site' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/site' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/grupy-pa.github.io' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/grupy-pa.github.io' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
